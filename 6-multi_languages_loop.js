4de64e5a23a0dea841fab7ea0e76f1a2230945d0
Anita Samuel <migist2@gmail.com>
2025-07-08 20:41:08 +0100

    for loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-const threeLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const threeLines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for(i = 0; i < threeLines.length; i++){
-    console.log(threeLines[i])
+for (i = 0; i < threeLines.length; i++) {
+  console.log(threeLines[i]);
 }
