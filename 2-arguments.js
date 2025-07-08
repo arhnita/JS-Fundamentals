e8286c8b29adb42e690c2e0d1cda42d8757b6e42
Anita Samuel <migist2@gmail.com>
2025-07-08 19:49:15 +0100

    check arguments

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+const checkArguments = (...args) => {
+  if (args.length === 0) {
+    console.log("No argument");
+  } else if (args.length === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
+};
+
+checkArguments();
+checkArguments("Hello");
+checkArguments("Hello", "Anita");
