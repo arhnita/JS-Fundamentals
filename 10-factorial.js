80cc1d127bbf36b67bcad029a8231c5d23e7b44c
Anita Samuel <migist2@gmail.com>
2025-07-09 20:17:12 +0100

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,15 @@
+const number = parseInt(process.argv[2]);
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  if (n < 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const result = factorial(number);
+console.log(result);
